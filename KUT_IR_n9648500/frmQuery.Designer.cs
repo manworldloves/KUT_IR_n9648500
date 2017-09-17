63a52d84698f1446672e335f60bb087bc6891ff3
manworldloves <themantheworldloves@gmail.com>
2017-09-18 07:28:48 +1000

    Added query form

---
+++ KUT_IR_n9648500/frmQuery.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace KUT_IR_n9648500
+{
+    partial class frmQuery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkProcess = new System.Windows.Forms.CheckBox();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.lblProcQuery = new System.Windows.Forms.Label();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chkProcess
+            // 
+            this.chkProcess.AutoSize = true;
+            this.chkProcess.Location = new System.Drawing.Point(22, 128);
+            this.chkProcess.Name = "chkProcess";
+            this.chkProcess.Size = new System.Drawing.Size(110, 17);
+            this.chkProcess.TabIndex = 0;
+            this.chkProcess.Text = "Preprocess Query";
+            this.chkProcess.UseVisualStyleBackColor = true;
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(22, 76);
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(544, 20);
+            this.txtQuery.TabIndex = 1;
+            // 
+            // lblProcQuery
+            // 
+            this.lblProcQuery.AutoSize = true;
+            this.lblProcQuery.Location = new System.Drawing.Point(22, 178);
+            this.lblProcQuery.Name = "lblProcQuery";
+            this.lblProcQuery.Size = new System.Drawing.Size(0, 13);
+            this.lblProcQuery.TabIndex = 2;
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(201, 128);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(161, 23);
+            this.btnQuery.TabIndex = 3;
+            this.btnQuery.Text = "Search";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            // 
+            // frmQuery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 307);
+            this.Controls.Add(this.btnQuery);
+            this.Controls.Add(this.lblProcQuery);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.chkProcess);
+            this.Name = "frmQuery";
+            this.Text = "frmQuery";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkProcess;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Label lblProcQuery;
+        private System.Windows.Forms.Button btnQuery;
+    }
+}
